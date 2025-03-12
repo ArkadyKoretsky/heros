7595489f291da4a1c3a00c351ae0e3e8e246d240
Arkady Koretsky <arkady1@rafael.co.il>
2025-03-12 16:20:25 +0200

    [FIX] unit testing of schedulers

--- apps/schedulers/src/schedulers.controller.spec.ts
+++ apps/schedulers/src/schedulers.controller.spec.ts
@@ -3,15 +3,25 @@ import { SchedulersController } from './schedulers.controller';
 import { SchedulersService } from './schedulers.service';
 import { schedulersServiceInfo } from './utils';
 import { ServiceInfoModule } from '@shield/common';
+import { SuperheroTask } from './entities';
 
 describe(SchedulersController.name, () => {
+  const tasksQueue: SuperheroTask[] = [];
   let schedulersController: SchedulersController;
 
   beforeEach(async () => {
+    const schedulersServiceMock = {
+      addSuperheroTask: (task: SuperheroTask): SuperheroTask => {
+        tasksQueue.push(task);
+        return task;
+      },
+    };
     const app: TestingModule = await Test.createTestingModule({
       imports: [ServiceInfoModule.register(schedulersServiceInfo)],
       controllers: [SchedulersController],
-      providers: [SchedulersService],
+      providers: [
+        { provide: SchedulersService, useValue: schedulersServiceMock },
+      ],
     }).compile();
 
     schedulersController = app.get<SchedulersController>(SchedulersController);
@@ -19,6 +29,14 @@ describe(SchedulersController.name, () => {
 
   describe('root', () => {
     it('should return schedulers service info', () => {
+      expect(schedulersController.getSchedulersServiceInfo()).toBe(
+        schedulersServiceInfo,
+      );
+    });
+  });
+
+  describe('add task to queue', () => {
+    it('should add task to ', () => {
       expect(schedulersController.getSchedulersServiceInfo()).toBe(
         schedulersServiceInfo,
       );
